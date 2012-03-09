042fc558831004048d223a469666c9671835d145
Troy Melhase <tmelhase@ngs.org>
2012-03-09 09:03:48 -0500

    Copies files and approach from rmm5t/dotfiles.

---
+++ emacs.d/natural/custom.el
@@ -0,0 +1,58 @@
+;; added by Custom
+;
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(browse-url-browser-function (quote browse-url-kde))
+ '(c-basic-offset 4)
+ '(c-insert-tab-function (quote insert-tab))
+ '(c-syntactic-indentation nil)
+ '(gud-gdb-command-name "gdb --annotate=1")
+ '(indent-tabs-mode nil)
+ '(inhibit-startup-screen t)
+ '(ipython-command "/usr/local/bin/ipython")
+ '(large-file-warning-threshold nil)
+ '(nxml-child-indent 4)
+ '(nxml-outline-child-indent 4)
+ '(py-python-command "/usr/local/bin/ipython")
+ '(sgml-basic-offset 4)
+ '(show-trailing-whitespace t)
+ '(w3m-default-display-inline-images t)
+ '(w3m-use-toolbar nil))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "apple" :family "Monaco"))))
+ '(antlr-keyword ((t (:inherit font-lock-keyword-face :foreground "#B09378"))))
+ '(antlr-ruledef ((t (:inherit font-lock-function-name-face :foreground "#587EB0"))))
+ '(antlr-tokenref ((t (:inherit font-lock-type-face :foreground "#ffc080"))))
+ '(buffer-menu-buffer ((t (:foreground "#9AC0CD"))))
+ '(completions-first-difference ((t (:inherit bold :foreground "#FFB90F"))))
+ '(custom-link ((t (:inherit link))))
+ '(custom-state ((((class color) (background dark)) (:foreground "#1874cd"))))
+ '(eshell-prompt ((t (:foreground "#79B579"))))
+ '(font-lock-comment-delimiter-face ((default (:foreground "#999999")) (((class color) (min-colors 16)) nil)))
+ '(font-lock-comment-face ((((class color) (min-colors 88) (background dark)) (:foreground "#999999"))))
+ '(font-lock-function-name-face ((((class color) (min-colors 88) (background dark)) (:foreground "#ffc080" :weight bold))))
+ '(font-lock-keyword-face ((((class color) (min-colors 88) (background dark)) (:foreground "#587EB0"))))
+ '(font-lock-string-face ((((class color) (min-colors 88) (background dark)) (:foreground "#7Fb094"))))
+ '(font-lock-type-face ((((class color) (min-colors 88) (background dark)) (:foreground "#B09378" :weight bold))))
+ '(fringe ((((class color) (background dark)) (:background "#000000"))))
+ '(isearch ((((class color) (min-colors 88) (background dark)) (:background "#ff4500" :foreground "#ffffff"))))
+ '(lazy-highlight ((((class color) (min-colors 88) (background dark)) (:background "#caff70" :foreground "black"))))
+ '(link ((((class color) (min-colors 88) (background dark)) (:foreground "#1E90FF" :underline t))))
+ '(minibuffer-prompt ((((background dark)) (:foreground "#1E90FF"))))
+ '(mode-line ((((class color) (min-colors 88)) (:background "#000000" :foreground "#ffffff" :box (:line-width -1 :color "#1E90FF")))))
+ '(mode-line-buffer-id ((t (:foreground "#1E90FF"))))
+ '(mode-line-emphasis ((t nil)))
+ '(mode-line-inactive ((t (:background "#000000" :foreground "#505050" :box (:line-width -1 :color "#909090")))))
+ '(py-builtins-face ((t (:foreground "#98ff7c"))))
+ '(py-decorators-face ((t (:foreground "#ff4500"))))
+ '(py-pseudo-keyword-face ((t (:foreground "#6caad9"))))
+ '(region ((((class color) (min-colors 88) (background dark)) (:background "#1874CD" :foreground "#ffffff"))))
+ '(scroll-bar ((t (:foreground "red"))))
+ '(trailing-whitespace ((((class color) (background dark)) (:background "#303030")))))
---
+++ emacs.d/natural/defuns.el
@@ -0,0 +1,112 @@
+;;; Personal functions
+
+;; For loading libraries from the vendor directory
+;; Modified from defunkt's original version to support autoloading.
+;; http://github.com/defunkt/emacs/blob/master/defunkt/defuns.el
+(defun vendor (library &rest autoload-functions)
+  (let* ((file (symbol-name library))
+         (normal (concat "~/.emacs.d/vendor/" file))
+         (suffix (concat normal ".el"))
+         (personal (concat "~/.emacs.d/natural/" file))
+	 (found nil))
+    (cond
+     ((file-directory-p normal) (add-to-list 'load-path normal) (set 'found t))
+     ((file-directory-p suffix) (add-to-list 'load-path suffix) (set 'found t))
+     ((file-exists-p suffix)  (set 'found t)))
+    (when found
+      (if autoload-functions
+          (dolist (autoload-function autoload-functions)
+            (autoload autoload-function (symbol-name library) nil t))
+        (require library)))
+    (when (file-exists-p (concat personal ".el"))
+      (load personal))))
+
+;; Arrows are common, especially in ruby
+(defun insert-arrow ()
+  (interactive)
+  (delete-horizontal-space)
+  (insert " => "))
+
+;; Quickly jump back and forth between matching parens/brackets
+(defun match-paren (arg)
+  "Go to the matching parenthesis if on parenthesis."
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))))
+
+;; Make the whole buffer pretty and consistent
+(defun iwb()
+  "Indent Whole Buffer"
+  (interactive)
+  (delete-trailing-whitespace)
+  (indent-region (point-min) (point-max) nil)
+  (untabify (point-min) (point-max)))
+
+(defun delete-window-replacement (&optional p)
+  "Kill current window.  If called with PREFIX, kill the buffer too."
+  (interactive "P")
+  (if p
+      (kill-buffer nil))
+  (delete-window))
+
+(defun delete-other-windows-replacement (&optional p)
+  "Make the selected window fill its frame.  If called with PREFIX,
+kill all other visible buffers."
+  (interactive "P")
+  (if p
+      (dolist (window (window-list))
+        (unless (equal (window-buffer window) (current-buffer))
+          (kill-buffer (window-buffer window)))))
+  (delete-other-windows))
+
+(defun lorem ()
+  (interactive)
+  (insert "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent libero orci, auctor sed, faucibus vestibulum, gravida vitae, arcu. Nunc posuere. Suspendisse potenti. Praesent in arcu ac nisl ultricies ultricies. Fusce eros. Sed pulvinar vehicula ante. Maecenas urna dolor, egestas vel, tristique et, porta eu, leo. Curabitur vitae sem eget arcu laoreet vulputate. Cras orci neque, faucibus et, rhoncus ac, venenatis ac, magna. Aenean eu lacus. Aliquam luctus facilisis augue. Nullam fringilla consectetuer sapien. Aenean neque augue, bibendum a, feugiat id, lobortis vel, nunc. Suspendisse in nibh quis erat condimentum pretium. Vestibulum tempor odio et leo. Sed sodales vestibulum justo. Cras convallis pellentesque augue. In eu magna. In pede turpis, feugiat pulvinar, sodales eget, bibendum consectetuer, magna. Pellentesque vitae augue."))
+
+;; ;; Use the text around point as a cue what it is that we want from the
+;; ;; editor. Allowance has to be made for the case that point is at the
+;; ;; edge of a buffer.
+;; (defun indent-or-expand (arg)
+;;   "Either indent according to mode, or expand the word preceding
+;; point."
+;;   (interactive "*P")
+;;   (if (and
+;;        (or (bobp) (= ?w (char-syntax (char-before))))
+;;        (or (eobp) (not (= ?w (char-syntax (char-after))))))
+;;       (dabbrev-expand arg)
+;;     (indent-according-to-mode)))
+
+;; This override for transpose-words fixes what I consider to be a flaw with the
+;; default implementation in simple.el. To traspose chars or lines, you always
+;; put the point on the second char or line to transpose with the previous char
+;; or line.  The default transpose-words implementation does the opposite by
+;; flipping the current word with the next word instead of the previous word.
+;; The new implementation below instead makes transpose-words more consistent
+;; with how transpose-chars and trasponse-lines behave.
+(defun transpose-words (arg)
+  "[Override for default transpose-words in simple.el]
+Interchange words around point, leaving point at end of
+them. With prefix arg ARG, effect is to take word before or
+around point and drag it backward past ARG other words (forward
+if ARG negative).  If ARG is zero, the words around or after
+point and around or after mark are interchanged."
+  (interactive "*p")
+  (if (eolp) (forward-char -1))
+  (transpose-subr 'backward-word arg)
+  (forward-word (+ arg 1)))
+
+;; Borrowed from https://gist.github.com/1415844
+;; Also see http://emacsworld.blogspot.com/2011/12/moving-buffers-between-windows.html
+(require 'cl)
+(defun rotate-left (l)
+  (append  (cdr l) (list (car l))))
+(defun rotate-windows ()
+  (interactive)
+  (let ((start-positions (rotate-left (mapcar 'window-start (window-list))))
+	(buffers (rotate-left (mapcar 'window-buffer (window-list)))))
+    (mapcar* (lambda (window  buffer pos)
+	       (set-window-buffer window buffer)
+	       (set-window-start window pos))
+	     (window-list)
+	     buffers
+	     start-positions)))
